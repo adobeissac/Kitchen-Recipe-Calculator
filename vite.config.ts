c70b1eb3eea3ff4936b301d6dd8f0302068ea938
shyjuissac <shyjuissacalex@gmail.com>
2026-05-19 13:33:32 +0100

    first commit for the app

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0',   // ✅ required for Docker dev mode
+    port: 5173
+  },
+
+  preview: {
+    host: '0.0.0.0',   // ✅ required if using vite preview in container
+    port: 4173
+  },
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
